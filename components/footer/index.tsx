699bdae11f3680e3c4536c474774edc02a480e13
Ethan Bautista <eibautistat@gmail.com>
2022-05-04 23:27:01 -0500

    This is the beginning of something cool

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,13 @@
 import { FooterContainer } from './styled';
 
 const Footer = () => {
-  return <FooterContainer>THIS IS A FOOTER</FooterContainer>;
+  return (
+    <FooterContainer>
+      <a href="https://www.flaticon.com/free-icons/web-development" title="web development icons">
+        Web development icons created by surang - Flaticon
+      </a>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
